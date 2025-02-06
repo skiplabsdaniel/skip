9ef1ae5ae1e65c75e77b9d9eee2a3fa0272db435
LOPES Daniel <daniel@skiplabs.io>
2025-02-06 17:49:15 +0100

    Allows to define custom packages

--- prebuild.mjs
+++ prebuild.mjs
@@ -1,6 +1,53 @@
 import * as fs from "fs";
 import * as path from "path";
 import { fileURLToPath } from "url";
+import { parseArgs } from "util";
+import { exit } from "process";
+
+const argSchema = {
+  help: {
+    type: "boolean",
+    short: "h",
+    default: false,
+    help: "",
+  },
+  links: {
+    type: "string",
+    short: "l",
+    help: "To specific a json file with additional links.",
+  },
+};
+
+function printHelp() {
+  const thisBin = process.argv[1];
+  const flags = Object.entries(argSchema)
+    .map(([key, def]) => {
+      const shortName = def.short;
+      const val = shortName ? `-${shortName}, ` : "";
+      const help = def.help ? `\n      ${def.help}` : "";
+      const deflt = "default" in def ? ` Default: ${def.default}` : "";
+      return `${val}--${key}${help}${deflt}`;
+    })
+    .join("\n  ");
+  console.log(`Usage: ${thisBin} [Options] package1 package2 ... \n  ${flags}`);
+}
+
+const { values, positionals } = parseArgs({
+  options: argSchema,
+  tokens: true,
+  allowPositionals: true,
+});
+
+if (values.help) {
+  printHelp();
+  exit(0);
+}
+
+if (positionals.length <= 0) {
+  console.error("Almost one package must be specified.");
+  printHelp();
+  exit(1);
+}
 
 const symlinks = {
   "skiplang-std": "./skiplang/prelude/ts/binding/src",
@@ -14,8 +61,8 @@ const symlinks = {
 const filename = fileURLToPath(import.meta.url);
 const dirname = path.dirname(filename);
 
-export function link(packages) {
-  for (const [symlink, target] of Object.entries(symlinks)) {
+export function link(dirname, availables, packages) {
+  for (const [symlink, target] of Object.entries(availables)) {
     if (!packages.has(symlink)) continue;
     const rand = new Uint32Array(1);
     crypto.getRandomValues(rand);
@@ -25,12 +72,12 @@ export function link(packages) {
   }
 }
 
-if (process.argv.length <= 2) {
-  const thisBin = process.argv[1];
-  console.log(`Usage: ${thisBin} <worspace-package>...`);
-  process.exit(0);
+const packages = new Set(positionals);
+
+link(dirname, symlinks, packages);
+
+if (values.links) {
+  const customlinks = JSON.parse(fs.readFileSync(values.links));
+  const customdir = path.dirname(path.resolve(values.links));
+  link(customdir, customlinks, packages);
 }
-
-const packages = new Set(process.argv.slice(2));
-
-link(packages);
