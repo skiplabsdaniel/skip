6939ead9eb681d7428503136e581de2a017e4fa9
LOPES Daniel <daniel@skiplabs.io>
2025-02-07 17:48:23 +0100

    [skjson] Manage typed object

--- skipruntime-ts/addon/src/cjson.cc
+++ skipruntime-ts/addon/src/cjson.cc
@@ -20,7 +20,7 @@ CJSON SKIP_SKJSON_createCJFloat(double v);
 CJSON SKIP_SKJSON_createCJString(char* str);
 CJSON SKIP_SKJSON_createCJBool(bool v);
 
-double SKIP_SKJSON_typeOf(CJSON json);
+char* SKIP_SKJSON_typeOf(CJSON json);
 double SKIP_SKJSON_asNumber(CJSON json);
 int32_t SKIP_SKJSON_asBoolean(CJSON json);
 char* SKIP_SKJSON_asString(CJSON json);
@@ -277,6 +277,39 @@ void CreateCJBool(const FunctionCallbackInfo<Value>& args) {
   });
 }
 
+void CreateCJTyped(const FunctionCallbackInfo<Value>& args) {
+  Isolate* isolate = args.GetIsolate();
+  if (args.Length() != 2) {
+    // Throw an Error that is passed back to JavaScript
+    isolate->ThrowException(
+        Exception::TypeError(FromUtf8(isolate, "Must have two parameters.")));
+    return;
+  };
+  if (!args[0]->IsExternal()) {
+    // Throw an Error that is passed back to JavaScript
+    isolate->ThrowException(Exception::TypeError(
+        FromUtf8(isolate, "The first parameter must be a pointer.")));
+    return;
+  }
+  if (!args[1]->IsString()) {
+    // Throw an Error that is passed back to JavaScript
+    isolate->ThrowException(Exception::TypeError(
+        FromUtf8(isolate, "The second parameter must be a string.")));
+    return;
+  }
+
+  NatTryCatch(isolate, [&args](Isolate* isolate) {
+    CJSON sktyped =
+        SKIP_SKJSON_createCJTyped(args[0].As<External>()->Value(),
+                                  ToSKString(isolate, args[1].As<String>()));
+    args.GetReturnValue().Set(External::New(isolate, sktyped));
+  });
+}
+
+bool IsNumber(const std::string& s) {
+  return !s.empty() && std::all_of(s.begin(), s.end(), ::isdigit);
+}
+
 void TypeOf(const FunctionCallbackInfo<Value>& args) {
   Isolate* isolate = args.GetIsolate();
   if (args.Length() != 1) {
@@ -292,8 +325,12 @@ void TypeOf(const FunctionCallbackInfo<Value>& args) {
     return;
   };
   NatTryCatch(isolate, [&args](Isolate* isolate) {
-    double sktype = SKIP_SKJSON_typeOf(args[0].As<External>()->Value());
-    args.GetReturnValue().Set(Number::New(isolate, sktype));
+    char* sktype = SKIP_SKJSON_typeOf(args[0].As<External>()->Value());
+    if (IsNumber(sktype)) {
+      args.GetReturnValue().Set(Number::New(isolate, atoi(sktype)));
+    } else {
+      args.GetReturnValue().Set(FromUtf8(isolate, sktype));
+    }
   });
 }
 
