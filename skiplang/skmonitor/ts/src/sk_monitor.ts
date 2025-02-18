skiplabsdaniel/skdb

[skmonitor:ts] Monitoring for client

skmonitor/ts/src/sk_monitor.ts
@@ -1,6 +1,19 @@
 // sknpm: Cannot be multiline for package sources
 // prettier-ignore
-import type { int, ptr, float, Environment, Links, ToWasmManager, Utils, Opt } from "#std/sk_types.js";
+import type { ptr, float, Opt, Environment, Links, ToWasmManager, Utils, Shared } from "#std/sk_types.js";
+import type {
+  Attribute,
+  AttributeValue,
+  ScalarValue,
+} from "./sk_monitor_common.js";
+import { Monitor } from "./sk_monitor_monitor.js";
+import {
+  type Span,
+  type Event,
+  SpanKind,
+  type SpanStatus,
+  SpanStatusCode,
+} from "./sk_monitor_span.js";
 
 interface ToWasm {
   SKIP_SKMonitor_write: (content: ptr) => void;
@@ -9,33 +22,185 @@ interface ToWasm {
   SKIP_JS_now: () => float;
 }
 
-class LinksImpl implements Links {
-  write!: (content: ptr) => int;
-  traceIdOpt!: () => Opt<ptr>;
-  monitoringOpt!: () => Opt<ptr>;
+export type Collector = (span: Span) => void;
 
-  constructor() {}
+export type Builder = (monitor: Monitor) => Collector[];
 
-  complete = (utils: Utils, exports: object) => {
-    this.write = (content: ptr) => {
-      console.log(utils.importString(content));
-      return 1;
-    };
-    this.traceIdOpt = () => {
-      return null;
-    };
-    this.monitoringOpt = () => {
-      return null;
-    };
+export type Config = {
+  maxSize?: number;
+  traceId?: string;
+  worker?: Worker;
+  builder?: Builder;
+};
+
+type SkIntScalar = { int: string };
+type SkScalar = string | number | boolean | SkIntScalar;
+type SkValue = SkScalar | SkScalar[];
+
+interface SkAttributes {
+  [attributeKey: string]: SkValue;
+}
+
+type SkTime = [string, string];
+type SkService = { name: string; version: string };
+type SkStatus = { status: string; message: string };
+type SkEvent = { name: string; attributes: SkAttributes; time: SkTime };
+
+type SkSpan = {
+  start: SkTime;
+  traceId: string;
+  spanId: string;
+  name: string;
+  end: SkTime;
+  attributes: SkAttributes;
+  events: SkEvent[];
+  parentId?: string;
+  service?: SkService;
+  status?: SkStatus;
+};
+
+function convertValue(skValue: SkValue): AttributeValue {
+  const type = typeof skValue;
+  if (type === "string") {
+    return { stringValue: skValue as string };
+  } else if (type === "number") {
+    return { doubleValue: skValue as number };
+  } else if (type === "boolean") {
+    return { boolValue: skValue as boolean };
+  } else if (Array.isArray(skValue)) {
+    const values = (skValue as SkValue[]).map(
+      (v) => convertValue(v) as ScalarValue,
+    );
+    return { arrayValue: { values } };
+  }
+  const intVal = skValue as SkIntScalar;
+  return { intValue: intVal.int };
+}
+
+function convertAttributes(skAttributes: SkAttributes): Attribute[] {
+  const keys = Object.keys(skAttributes);
+  const attributes: Attribute[] = [];
+  for (const key in keys) {
+    attributes.push({ key: key, value: convertValue(skAttributes[key]) });
+  }
+  return attributes;
+}
+
+function convertEvent(skEvent: SkEvent): Event {
+  return {
+    name: skEvent.name,
+    attributes: convertAttributes(skEvent.attributes),
+    timeUnixNano: skEvent.time[0] + skEvent.time[1],
+    droppedAttributesCount: 0,
   };
 }
 
+function convertStatus(skStatus: SkStatus): SpanStatus {
+  switch (skStatus.status) {
+    case "Ok": {
+      return { code: SpanStatusCode.OK };
+    }
+    case "Error": {
+      return { code: SpanStatusCode.ERROR, message: skStatus.message };
+    }
+    case "Unset":
+    default: {
+      return { code: SpanStatusCode.UNSET };
+    }
+  }
+}
+
+class SKMonitorShared implements Shared {
+  getName = () => "SKMonitor";
+
+  config?: Config;
+  collectors?: Collector[];
+
+  monitor(config: Config) {
+    this.config = config;
+    if (this.config.builder) {
+      const monitor = new Monitor(config.maxSize);
+      this.collectors = this.config.builder(monitor);
+    }
+  }
+
+  traceId(): Opt<string> {
+    const traceId = this.config ? this.config.traceId : undefined;
+    return traceId ?? null;
+  }
+
+  monitorMode(): Opt<string> {
+    const trace = this.config ? this.config.builder !== undefined : undefined;
+    if (trace === undefined) return null;
+    let strMode: string = "memory";
+    if (trace) {
+      strMode = "on";
+    }
+    return strMode;
+  }
+
+  collect(skSpan: SkSpan) {
+    if (!this.collectors) return;
+    const span = {
+      kind: SpanKind.CLIENT,
+      name: skSpan.name,
+      traceId: skSpan.traceId,
+      spanId: skSpan.spanId,
+      parentSpanId: skSpan.parentId ?? "",
+      attributes: convertAttributes(skSpan.attributes),
+      events: skSpan.events.map((e) => convertEvent(e)),
+      status: skSpan.status ? convertStatus(skSpan.status) : undefined,
+      startTimeUnixNano: skSpan.start[0] + skSpan.start[1],
+      endTimeUnixNano: skSpan.end[0] + skSpan.end[1],
+    };
+    this.collectors.forEach((c) => c(span));
+  }
+}
+
+class LinksImpl implements Links {
+  private environment: Environment;
+  private skmonitor: SKMonitorShared;
+
+  write!: (content: ptr) => void;
+  traceIdOpt!: () => Opt<ptr>;
+  monitoringOpt!: () => Opt<ptr>;
+
+  constructor(environment: Environment) {
+    this.environment = environment;
+    this.skmonitor = new SKMonitorShared();
+  }
+
+  complete = (utils: Utils, exports: object) => {
+    this.write = (skContent: ptr) => {
+      if (!this.skmonitor.monitor) return;
+      this.collect(utils.importString(skContent));
+    };
+    this.traceIdOpt = () => {
+      const traceId = this.skmonitor.traceId();
+      return traceId ? utils.exportString(traceId) : null;
+    };
+    this.monitoringOpt = () => {
+      const mode = this.skmonitor.monitorMode();
+      return mode ? utils.exportString(mode) : null;
+    };
+    this.environment.shared.set("SKMonitor", this.skmonitor);
+  };
+
+  private async collect(strSkSpan: string) {
+    const skSpan = JSON.parse(strSkSpan) as SkSpan;
+    this.skmonitor.collect(skSpan);
+  }
+}
+
 class Manager implements ToWasmManager {
-  constructor() {}
+  private environment: Environment;
+  constructor(environment: Environment) {
+    this.environment = environment;
+  }
 
   prepare = (wasm: object) => {
     let toWasm = wasm as ToWasm;
-    let links = new LinksImpl();
+    let links = new LinksImpl(this.environment);
     toWasm.SKIP_SKMonitor_write = (content: ptr) => links.write(content);
     toWasm.SKIP_SKMonitor_traceIdOpt = () => links.traceIdOpt();
     toWasm.SKIP_SKMonitor_monitoringOpt = () => links.monitoringOpt();
@@ -45,6 +210,6 @@ class Manager implements ToWasmManager {
 }
 
 /** @sk init */
-export function init(env?: Environment) {
-  return Promise.resolve(new Manager());
+export function init(env: Environment) {
+  return Promise.resolve(new Manager(env));
 }

skmonitor/ts/src/sk_monitor_exporter.ts
@@ -1,0 +1,225 @@
+import type { Metrics } from "./sk_monitor_metric.js";
+import type { TraceExporter, MetricsExporter } from "./sk_monitor_monitor.js";
+import type { Trace } from "./sk_monitor_span.js";
+import type { Headers } from "./sk_monitor_common.js";
+
+abstract class HttpJSONExporter {
+  protected abstract url(): URL;
+
+  protected headers(): Headers {
+    return {};
+  }
+
+  protected post(value: object) {
+    const self = this;
+    const request = new XMLHttpRequest();
+    request.open("POST", this.url(), true);
+    request.onload = function () {
+      if (this.status < 200 && this.status >= 400) {
+        // Error!
+        self.failure(value, this.status);
+      }
+    };
+
+    request.onerror = function (err: ProgressEvent) {
+      // There was a connection error of some sort
+      // TODO retry
+      self.failure(value, 0, err);
+    };
+    request.setRequestHeader("Content-Type", "application/json");
+    const headers = this.headers();
+    const keys = Object.keys(this.headers());
+    for (const key in keys) {
+      request.setRequestHeader(key, headers[key]);
+    }
+    request.send(JSON.stringify(value));
+  }
+
+  protected failure(
+    value: object,
+    code: number,
+    err?: ProgressEvent<EventTarget>,
+  ): void {
+    if (err) console.error(err);
+    else console.error(`Monitor trace export failed with code: ${code}`);
+  }
+}
+
+export type Product = {
+  id: number;
+  token: string;
+};
+
+abstract class OptlHttpJSONExporterBase extends HttpJSONExporter {
+  private _headers: Headers;
+  private _url: URL;
+
+  constructor(url: URL, headers: Headers) {
+    super();
+    this._url = url;
+    this._headers = headers;
+  }
+
+  protected abstract path(): string;
+
+  protected url(): URL {
+    return new URL(this.path(), this._url);
+  }
+
+  protected override headers(): Headers {
+    return this._headers;
+  }
+}
+
+const uptrace_dsn = (url: URL, product: Product) => {
+  const uptrace_dsn = new URL(url, product.id.toString());
+  uptrace_dsn.username = product.token;
+  return uptrace_dsn;
+};
+
+abstract class UptraceHttpJSONExporter extends OptlHttpJSONExporterBase {
+  constructor(url: URL, product: Product) {
+    super(url, {
+      "uptrace-dsn": uptrace_dsn(url, product).toString(),
+    });
+  }
+}
+
+export class UptraceTraceHttpJSONExporter
+  extends UptraceHttpJSONExporter
+  implements TraceExporter
+{
+  private _path: string;
+
+  constructor(url: URL, product: Product, version: number = 2) {
+    super(url, product);
+    this._path = `/api/v${version}/spans`;
+  }
+
+  export(trace: Trace): void {
+    this.post(trace);
+  }
+
+  protected path() {
+    return this._path;
+  }
+}
+
+export class OptlTraceHttpJSONExporter
+  extends OptlHttpJSONExporterBase
+  implements TraceExporter
+{
+  private _path: string;
+
+  constructor(url: URL, headers: Headers = {}, version: number = 2) {
+    super(url, headers);
+    this._path = `/api/v${version}/spans`;
+  }
+
+  export(trace: Trace): void {
+    this.post(trace);
+  }
+
+  protected path() {
+    return this._path;
+  }
+}
+
+export class UptraceMetricsHttpJSONExporter
+  extends UptraceHttpJSONExporter
+  implements MetricsExporter
+{
+  private _path: string;
+
+  constructor(url: URL, product: Product, version: number = 2) {
+    super(url, product);
+    this._path = `/api/v${version}/metrics`;
+  }
+
+  export(metrics: Metrics): void {
+    this.post(metrics);
+  }
+
+  protected path() {
+    return this._path;
+  }
+}
+
+export class OptlMetricsHttpJSONExporter
+  extends OptlHttpJSONExporterBase
+  implements MetricsExporter
+{
+  private _path: string;
+
+  constructor(url: URL, headers: Headers = {}, version: number = 2) {
+    super(url, headers);
+    this._path = `/api/v${version}/metrics`;
+  }
+
+  export(metrics: Metrics): void {
+    this.post(metrics);
+  }
+
+  protected path() {
+    return this._path;
+  }
+}
+
+export class ConsoleTraceExporter implements TraceExporter {
+  export(trace: Trace): void {
+    console.log(trace);
+  }
+}
+
+export class ConsoleMetricsExporter implements MetricsExporter {
+  export(metrics: Metrics): void {
+    console.log(metrics);
+  }
+}
+
+abstract class IndexedDBExporter {
+  protected db: IDBDatabase;
+
+  constructor(db: IDBDatabase) {
+    this.db = db;
+  }
+
+  protected abstract name(): string;
+
+  protected insert(value: object) {
+    const name = this.name();
+    const transaction = this.db.transaction([name], "readwrite");
+    // Handler for any unexpected error
+    transaction.onerror = () => {
+      console.error(transaction.error);
+    };
+    const objectStore = transaction.objectStore(name);
+    objectStore.add(value);
+  }
+}
+
+export class IndexedDBTraceExporter
+  extends IndexedDBExporter
+  implements TraceExporter
+{
+  export(trace: Trace): void {
+    this.insert(trace);
+  }
+
+  protected name() {
+    return "trace";
+  }
+}
+
+export class IndexedDBMetricsExporter
+  extends IndexedDBExporter
+  implements MetricsExporter
+{
+  export(metrics: Metrics): void {
+    this.insert(metrics);
+  }
+
+  protected name() {
+    return "metrics";
+  }
+}

skiplabsdaniel/skip

skiplang/skmonitor/ts/src/sk_monitor.ts
@@ -1,6 +1,5 @@
 import type * as Internal from "../skiplang-std/internal.js";
 import type {
-  int,
   ptr,
   float,
   Environment,
@@ -8,7 +7,23 @@ import type {
   ToWasmManager,
   Utils,
   Nullable,
+  Shared,
 } from "../skipwasm-std/index.js";
+import {
+  SpanKind,
+  SpanStatusCode,
+  type Span,
+  type SpanStatus,
+  type Event,
+} from "./sk_monitor_span.js";
+import { Monitor } from "./sk_monitor_monitor.js";
+import type {
+  Attribute,
+  AttributeValue,
+  ScalarValue,
+} from "./sk_monitor_common.js";
+
+export { Monitor };
 
 interface ToWasm {
   SKIP_SKMonitor_write: (content: ptr<Internal.String>) => void;
@@ -17,33 +32,188 @@ interface ToWasm {
   SKIP_JS_now: () => float;
 }
 
-class LinksImpl implements Links {
-  write!: (content: ptr<Internal.String>) => int;
-  traceIdOpt!: () => Nullable<ptr<Internal.String>>;
-  monitoringOpt!: () => Nullable<ptr<Internal.String>>;
+export type Collector = (span: Span) => void;
 
-  complete = (utils: Utils, _exports: object) => {
-    this.write = (content: ptr<Internal.String>) => {
-      console.log(utils.importString(content));
-      return 1;
-    };
-    this.traceIdOpt = () => {
-      return null;
-    };
-    this.monitoringOpt = () => {
-      return null;
-    };
+export type Builder = (monitor: Monitor) => Collector[];
+
+export type Config = {
+  maxSize?: number;
+  traceId?: string;
+  worker?: Worker;
+  builder?: Builder;
+};
+
+type SkIntScalar = { int: string };
+type SkScalar = string | number | boolean | SkIntScalar;
+type SkValue = SkScalar | SkScalar[];
+
+interface SkAttributes {
+  [attributeKey: string]: SkValue;
+}
+
+type SkTime = [string, string];
+type SkService = { name: string; version: string };
+type SkStatus = { status: string; message: string };
+type SkEvent = { name: string; attributes: SkAttributes; time: SkTime };
+
+type SkSpan = {
+  start: SkTime;
+  traceId: string;
+  spanId: string;
+  name: string;
+  end: SkTime;
+  attributes: SkAttributes;
+  events: SkEvent[];
+  parentId?: string;
+  service?: SkService;
+  status?: SkStatus;
+};
+
+function convertValue(skValue: SkValue): AttributeValue {
+  const type = typeof skValue;
+  if (type === "string") {
+    return { stringValue: skValue as string };
+  } else if (type === "number") {
+    return { doubleValue: skValue as number };
+  } else if (type === "boolean") {
+    return { boolValue: skValue as boolean };
+  } else if (Array.isArray(skValue)) {
+    const values = (skValue as SkValue[]).map(
+      (v) => convertValue(v) as ScalarValue,
+    );
+    return { arrayValue: { values } };
+  }
+  const intVal = skValue as SkIntScalar;
+  return { intValue: intVal.int };
+}
+
+function convertAttributes(skAttributes: SkAttributes): Attribute[] {
+  const keys = Object.keys(skAttributes);
+  const attributes: Attribute[] = [];
+  for (const key of keys) {
+    // eslint-disable-next-line @typescript-eslint/no-unnecessary-type-assertion
+    const value = convertValue(skAttributes[key]!);
+    attributes.push({ key, value });
+  }
+  return attributes;
+}
+
+function convertEvent(skEvent: SkEvent): Event {
+  return {
+    name: skEvent.name,
+    attributes: convertAttributes(skEvent.attributes),
+    timeUnixNano: skEvent.time[0] + skEvent.time[1],
+    droppedAttributesCount: 0,
   };
 }
 
+function convertStatus(skStatus: SkStatus): SpanStatus {
+  switch (skStatus.status) {
+    case "Ok": {
+      return { code: SpanStatusCode.OK };
+    }
+    case "Error": {
+      return { code: SpanStatusCode.ERROR, message: skStatus.message };
+    }
+    case "Unset":
+    default: {
+      return { code: SpanStatusCode.UNSET };
+    }
+  }
+}
+
+class SKMonitorShared implements Shared {
+  getName = () => "SKMonitor";
+
+  config?: Config;
+  collectors?: Collector[];
+
+  monitor(config: Config) {
+    this.config = config;
+    if (this.config.builder) {
+      const monitor = new Monitor(config.maxSize);
+      this.collectors = this.config.builder(monitor);
+    }
+  }
+
+  traceId(): Nullable<string> {
+    const traceId = this.config ? this.config.traceId : undefined;
+    return traceId ?? null;
+  }
+
+  monitorMode(): Nullable<string> {
+    const trace = this.config ? this.config.builder !== undefined : undefined;
+    if (trace === undefined) return null;
+    let strMode: string = "memory";
+    if (trace) {
+      strMode = "on";
+    }
+    return strMode;
+  }
+
+  collect(skSpan: SkSpan) {
+    if (!this.collectors) return;
+    const span = {
+      kind: SpanKind.CLIENT,
+      name: skSpan.name,
+      traceId: skSpan.traceId,
+      spanId: skSpan.spanId,
+      parentSpanId: skSpan.parentId ?? "",
+      attributes: convertAttributes(skSpan.attributes),
+      events: skSpan.events.map((e) => convertEvent(e)),
+      status: skSpan.status ? convertStatus(skSpan.status) : undefined,
+      startTimeUnixNano: skSpan.start[0] + skSpan.start[1],
+      endTimeUnixNano: skSpan.end[0] + skSpan.end[1],
+    };
+    this.collectors.forEach((c) => c(span));
+  }
+}
+
+class LinksImpl implements Links {
+  private environment: Environment;
+  private skmonitor: SKMonitorShared;
+
+  write!: (content: ptr<Internal.String>) => void;
+  traceIdOpt!: () => Nullable<ptr<Internal.String>>;
+  monitoringOpt!: () => Nullable<ptr<Internal.String>>;
+
+  constructor(environment: Environment) {
+    this.environment = environment;
+    this.skmonitor = new SKMonitorShared();
+  }
+
+  complete = (utils: Utils, _exports: object) => {
+    this.write = (skContent: ptr<Internal.String>) => {
+      if (!this.skmonitor.monitorMode()) return;
+      this.collect(utils.importString(skContent));
+    };
+    this.traceIdOpt = () => {
+      const traceId = this.skmonitor.traceId();
+      return traceId !== null ? utils.exportString(traceId) : null;
+    };
+    this.monitoringOpt = () => {
+      const mode = this.skmonitor.monitorMode();
+      return mode !== null ? utils.exportString(mode) : null;
+    };
+    this.environment.shared.set("SKMonitor", this.skmonitor);
+  };
+
+  private collect(strSkSpan: string) {
+    const skSpan = JSON.parse(strSkSpan) as SkSpan;
+    this.skmonitor.collect(skSpan);
+  }
+}
+
 class Manager implements ToWasmManager {
+  constructor(private environment: Environment) {}
+
   prepare = (wasm: object) => {
     const toWasm = wasm as ToWasm;
-    const links = new LinksImpl();
+    const links = new LinksImpl(this.environment);
     toWasm.SKIP_SKMonitor_write = (content: ptr<Internal.String>) =>
       links.write(content);
-    toWasm.SKIP_SKMonitor_traceIdOpt = () => links.traceIdOpt();
-    toWasm.SKIP_SKMonitor_monitoringOpt = () => links.monitoringOpt();
+    toWasm.SKIP_SKMonitor_traceIdOpt = links.traceIdOpt.bind(links);
+    toWasm.SKIP_SKMonitor_monitoringOpt = links.monitoringOpt.bind(links);
     toWasm.SKIP_JS_now = () => Date.now() / 1000;
     return links;
   };
@@ -52,9 +222,9 @@ class Manager implements ToWasmManager {
 /* @sk init */
 /**
  * Init the Monitoring Wasm code manager according given environment
- * @param _env The current environnement
+ * @param env The current environnement
  * @returns The Monitoring Wasm code manager
  */
-export function init(_env?: Environment) {
-  return Promise.resolve(new Manager());
+export function init(env: Environment) {
+  return Promise.resolve(new Manager(env));
 }

skiplang/skmonitor/ts/src/sk_monitor_exporter.ts
@@ -1,0 +1,226 @@
+import type { Metrics } from "./sk_monitor_metric.js";
+import type { TraceExporter, MetricsExporter } from "./sk_monitor_monitor.js";
+import type { Trace } from "./sk_monitor_span.js";
+import type { Headers } from "./sk_monitor_common.js";
+
+abstract class HttpJSONExporter {
+  protected abstract url(): URL;
+
+  protected headers(): Headers {
+    return {};
+  }
+
+  protected post(value: object) {
+    const request = new XMLHttpRequest();
+    const failure = this.failure.bind(this);
+    request.open("POST", this.url(), true);
+    request.onload = function () {
+      if (this.status < 200 && this.status >= 400) {
+        failure(value, this.status);
+      }
+    };
+
+    request.onerror = function (err: ProgressEvent) {
+      // There was a connection error of some sort
+      // TODO retry
+      failure(value, 0, err);
+    };
+    request.setRequestHeader("Content-Type", "application/json");
+    const headers = this.headers();
+    const keys = Object.keys(this.headers());
+    for (const key of keys) {
+      // eslint-disable-next-line @typescript-eslint/no-unnecessary-type-assertion
+      const value = headers[key]!;
+      request.setRequestHeader(key, value);
+    }
+    request.send(JSON.stringify(value));
+  }
+
+  protected failure(
+    _value: object,
+    code: number,
+    err?: ProgressEvent<EventTarget>,
+  ): void {
+    if (err) console.error(err);
+    else console.error(`Monitor trace export failed with code: ${code}`);
+  }
+}
+
+export type Product = {
+  id: number;
+  token: string;
+};
+
+abstract class OptlHttpJSONExporterBase extends HttpJSONExporter {
+  private _headers: Headers;
+  private _url: URL;
+
+  constructor(url: URL, headers: Headers) {
+    super();
+    this._url = url;
+    this._headers = headers;
+  }
+
+  protected abstract path(): string;
+
+  protected url(): URL {
+    return new URL(this.path(), this._url);
+  }
+
+  protected override headers(): Headers {
+    return this._headers;
+  }
+}
+
+const uptrace_dsn = (url: URL, product: Product) => {
+  const uptrace_dsn = new URL(url, product.id.toString());
+  uptrace_dsn.username = product.token;
+  return uptrace_dsn;
+};
+
+abstract class UptraceHttpJSONExporter extends OptlHttpJSONExporterBase {
+  constructor(url: URL, product: Product) {
+    super(url, {
+      "uptrace-dsn": uptrace_dsn(url, product).toString(),
+    });
+  }
+}
+
+export class UptraceTraceHttpJSONExporter
+  extends UptraceHttpJSONExporter
+  implements TraceExporter
+{
+  private _path: string;
+
+  constructor(url: URL, product: Product, version: number = 2) {
+    super(url, product);
+    this._path = `/api/v${version}/spans`;
+  }
+
+  export(trace: Trace): void {
+    this.post(trace);
+  }
+
+  protected path() {
+    return this._path;
+  }
+}
+
+export class OptlTraceHttpJSONExporter
+  extends OptlHttpJSONExporterBase
+  implements TraceExporter
+{
+  private _path: string;
+
+  constructor(url: URL, headers: Headers = {}, version: number = 2) {
+    super(url, headers);
+    this._path = `/api/v${version}/spans`;
+  }
+
+  export(trace: Trace): void {
+    this.post(trace);
+  }
+
+  protected path() {
+    return this._path;
+  }
+}
+
+export class UptraceMetricsHttpJSONExporter
+  extends UptraceHttpJSONExporter
+  implements MetricsExporter
+{
+  private _path: string;
+
+  constructor(url: URL, product: Product, version: number = 2) {
+    super(url, product);
+    this._path = `/api/v${version}/metrics`;
+  }
+
+  export(metrics: Metrics): void {
+    this.post(metrics);
+  }
+
+  protected path() {
+    return this._path;
+  }
+}
+
+export class OptlMetricsHttpJSONExporter
+  extends OptlHttpJSONExporterBase
+  implements MetricsExporter
+{
+  private _path: string;
+
+  constructor(url: URL, headers: Headers = {}, version: number = 2) {
+    super(url, headers);
+    this._path = `/api/v${version}/metrics`;
+  }
+
+  export(metrics: Metrics): void {
+    this.post(metrics);
+  }
+
+  protected path() {
+    return this._path;
+  }
+}
+
+export class ConsoleTraceExporter implements TraceExporter {
+  export(trace: Trace): void {
+    console.log(trace);
+  }
+}
+
+export class ConsoleMetricsExporter implements MetricsExporter {
+  export(metrics: Metrics): void {
+    console.log(metrics);
+  }
+}
+
+abstract class IndexedDBExporter {
+  protected db: IDBDatabase;
+
+  constructor(db: IDBDatabase) {
+    this.db = db;
+  }
+
+  protected abstract name(): string;
+
+  protected insert(value: object) {
+    const name = this.name();
+    const transaction = this.db.transaction([name], "readwrite");
+    // Handler for any unexpected error
+    transaction.onerror = () => {
+      console.error(transaction.error);
+    };
+    const objectStore = transaction.objectStore(name);
+    objectStore.add(value);
+  }
+}
+
+export class IndexedDBTraceExporter
+  extends IndexedDBExporter
+  implements TraceExporter
+{
+  export(trace: Trace): void {
+    this.insert(trace);
+  }
+
+  protected name() {
+    return "trace";
+  }
+}
+
+export class IndexedDBMetricsExporter
+  extends IndexedDBExporter
+  implements MetricsExporter
+{
+  export(metrics: Metrics): void {
+    this.insert(metrics);
+  }
+
+  protected name() {
+    return "metrics";
+  }
+}