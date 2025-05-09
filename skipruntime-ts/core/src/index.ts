22e2c642d8e5cd5febc223facf288fc6640791b1
LOPES Daniel <daniel@skiplabs.io>
2025-05-09 08:59:05 +0200

    Naming

--- skipruntime-ts/core/src/index.ts
+++ skipruntime-ts/core/src/index.ts
@@ -1140,7 +1140,7 @@ export class ToBinding {
     const skjson = this.getJsonConverter();
     const object = this.handles.get(skobject);
     const name = object.name;
-    const params = object.params.map((v) => {
+    const parameters = object.params.map((v) => {
       if (v instanceof EagerCollectionImpl) {
         return {
           type: "collection",
@@ -1155,7 +1155,7 @@ export class ToBinding {
       }
       return { type: "data", value: v as Json };
     });
-    return skjson.exportJSON({ name, params });
+    return skjson.exportJSON({ name, parameters });
   }
 
   //
